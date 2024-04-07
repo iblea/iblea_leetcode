aaf2c38c4fced4cb38d28917a9b23d2961ae900a
Iblee <iblea@iasdf.com>
2024-04-07 13:26:21 +0900

    Time: 98 ms (5.77%), Space: 26.4 MB (8.32%) - LeetHub

---
+++ 0113-path-sum-ii/0113-path-sum-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        results = []
+        def traversal(root, lst):
+            if root is None:
+                return None
+
+            lst.append(root.val)
+            print(lst)
+
+            if sum(lst) == targetSum:
+                if root.left is None and root.right is None:
+                    results.append([lst[i] for i in range(len(lst))])
+                    # results.append(lst)
+                    # lst.pop()
+                    return None
+
+            left_list = [ lst[i] for i in range(len(lst)) ]
+            right_list = [ lst[i] for i in range(len(lst)) ]
+            traversal(root.left, left_list)
+            traversal(root.right, right_list)
+
+            # traversal(root.left, lst)
+            # traversal(root.right, lst)
+
+
+
+        traversal(root, [])
+        return results
+
