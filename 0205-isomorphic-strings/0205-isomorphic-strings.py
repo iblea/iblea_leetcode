fb5a59b663781fed9b6a67b320a6f33a69648cb3
Iblea <iblea@iasdf.com>
2024-08-05 21:09:06 +0900

    Time: 42 ms (59.40%), Space: 17 MB (15.45%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,7 +1,9 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
         s1 = [ ]
-        s2 = [ ]
         dic = {}
         cnt = 0
         for c in s:
@@ -11,9 +13,12 @@ class Solution:
             s1.append(dic[c])
         dic = {}
         cnt = 0
-        for c in t:
+        for i in range(len(t)):
+            c = t[i]
             if c not in dic:
                 dic[c] = cnt
                 cnt += 1
-            s2.append(dic[c])
-        return s1 == s2
+            if dic[c] != s1[i]:
+                return False
+        return True
+
