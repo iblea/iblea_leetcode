567216e05f6971684fc7fe358bcd166f62936617
Iblea <iblea@iasdf.com>
2025-03-10 10:24:10 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3208-alternating-groups-ii/3208-alternating-groups-ii.cpp
+++ 3208-alternating-groups-ii/3208-alternating-groups-ii.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
     int numberOfAlternatingGroups(vector<int>& colors, int k) {
-        int count = 0;
-        int color_size = colors.size();
-        vector<int> diff(color_size);
 
-        int tmp = color_size - 1;
+        int n = colors.size();
+        int count = 0;
+        int tmp = n - 1;
+        vector<int> diff(n);
+
         for (int i = 0; i < tmp; i++) {
             diff[i] = colors[i] != colors[i + 1] ? 1 : 0;
         }
         diff[tmp] = colors[tmp] != colors[0] ? 1 : 0;
-
+        // k = 3 이면, diff 합계가 2가 되어야 함.
+        int expected = k - 1;
         int diff_sum = 0;
-        for (int i = 0; i < k - 1; i++) {
+        for (int i = 0; i < expected; i++) {
             diff_sum += diff[i];
         }
 
-        // if (diff_sum == k - 1) {
-        //     count = 1;
-        // }
-
-        for (int i = 0; i < color_size; i++) {
-            diff_sum = diff_sum - diff[i] + diff[(i + k - 1) % color_size];
-            if (diff_sum == k - 1) {
+        for (int i = 0; i < tmp; i++) {
+            diff_sum = diff_sum - diff[i] + diff[(i + k - 1) % n];
+            if (diff_sum == expected) {
                 count++;
             }
         }
-
         return count;
     }
 };
