172eebf9dde50b1ba41c07b15ce47c9475cef4ae
iblea <iblea@iasdf.com>
2025-03-10 10:12:04 +0900

    leetcode 3306 - can't

--- practice_code/cant/fullcode.cpp
+++ practice_code/cant/fullcode.cpp
@@ -9,66 +9,93 @@
 #include <queue>
 #include <array>
 #include <any>
+#include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
 
 
-/* https://leetcode.com/problems/alternating-groups-ii/?envType=daily-question&envId=2025-03-09
- * @lc app=leetcode id=3208 lang=cpp
- * [3208] Alternating Groups II
+/* https://leetcode.com/problems/count-of-substrings-containing-every-vowel-and-k-consonants-ii/?envType=daily-question&envId=2025-03-10
+ * @lc app=leetcode id=3306 lang=cpp
+ * [3306] Count of Substrings Containing Every Vowel and K Consonants II
 
-There is a circle of red and blue tiles. You are given an array of integers colors and an integer k. The color of tile i is represented by colors[i]:
-colors[i] == 0 means that tile i is red.
-colors[i] == 1 means that tile i is blue.
-An alternating group is every k contiguous tiles in the circle with alternating colors (each tile in the group except the first and last one has a different color from its left and right tiles).
-Return the number of alternating groups.
-Note that since colors represents a circle, the first and the last tiles are considered to be next to each other.
+You are given a string word and a non-negative integer k.
+Return the total number of substrings of word that contain every vowel ('a', 'e', 'i', 'o', and 'u') at least once and exactly k consonants.
 
-빨간색과 파란색 타일로 이루어진 원이 있습니다. 정수 배열 colors와 정수 k가 주어집니다. 타일 i의 색상은 colors[i]로 표현됩니다:
-colors[i] == 0은 타일 i가 빨간색임을 의미합니다.
-colors[i] == 1은 타일 i가 파란색임을 의미합니다.
-교차 그룹은 원 내에서 색상이 번갈아 나타나는 연속된 k개의 타일입니다(그룹 내 첫 번째와 마지막 타일을 제외한 각 타일은 좌우 타일과 다른 색상을 가집니다).
-교차 그룹의 수를 반환하세요.
-참고로 colors는 원을 나타내므로 첫 번째 타일과 마지막 타일은 서로 인접해 있다고 간주합니다.
+문자열 word와 음이 아닌 정수 k가 주어집니다.
+모든 모음('a', 'e', 'i', 'o', 'u')을 각각 최소 한 번씩 포함하고 정확히 k개의 자음을 포함하는 word의 부분 문자열의 총 개수를 반환하세요.
 
 Constraints:
-3 <= colors.length <= 10**5
-0 <= colors[i] <= 1
-3 <= k <= colors.length
+5 <= word.length <= 2 * 10**5
+word consists only of lowercase English letters.
+0 <= k <= word.length - 5
  */
 
 // @lc code=start
 class Solution {
 public:
-    int numberOfAlternatingGroups(vector<int>& colors, int k) {
-        int count = 0;
-        int color_size = colors.size();
-        vector<int> diff(color_size);
+    bool isVowel(char ch) {
+        return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+    }
 
-        int tmp = color_size - 1;
-        for (int i = 0; i < tmp; i++) {
-            diff[i] = colors[i] != colors[i + 1] ? 1 : 0;
+    long long countOfSubstrings(string word, int k) {
+        int n = word.size();
+        unordered_map<char, int> vowels;
+        int consonantCount = 0;
+        long long result = 0;
+
+        // Precompute next consonant positions
+        vector<int> nextConsonant(n);
+        int lastConsonant = n;
+        for (int i = n - 1; i >= 0; i--) {
+            nextConsonant[i] = lastConsonant;
+            // 연속되는 모음이 있으면, 연속되는 모음의 마지막 위치를 저장
+            if (!isVowel(word[i])) lastConsonant = i;
         }
-        diff[tmp] = colors[tmp] != colors[0] ? 1 : 0;
 
-        int diff_sum = 0;
-        for (int i = 0; i < k - 1; i++) {
-            diff_sum += diff[i];
-        }
-
-        for (int i = 0; i < color_size; i++) {
-            diff_sum = diff_sum - diff[i] + diff[(i + k - 1) % color_size];
-            if (diff_sum == k - 1) {
-                count++;
+        // Sliding window
+        int left = 0, right = 0;
+        while (right < n) {
+            // Expand window
+            if (isVowel(word[right])) {
+                vowels[word[right]]++;
+            } else {
+                consonantCount++;
             }
+
+            // Shrink window if too many consonants
+            while (left <= right && consonantCount > k) {
+                if (isVowel(word[left])) {
+                    if (--vowels[word[left]] == 0) vowels.erase(word[left]);
+                } else {
+                    consonantCount--;
+                }
+                left++;
+            }
+
+            // Count valid substrings
+            while (left < right && vowels.size() == 5 && consonantCount == k) {
+                // 모음이 5개가 있으므로, 이후에는 모음이 몇개든 상관이 없다.
+                // 따라서 연속되는 모음의 개수만큼 부분 문자열을 만들 수 있으므로, 그만큼 모두 더해야 한다.
+                result += (nextConsonant[right] - right);
+                if (isVowel(word[left])) {
+                    vowels[word[left]]--;
+                    if (vowels[word[left]] == 0) vowels.erase(word[left]);
+                } else {
+                    consonantCount--;
+                }
+                left++;
+            }
+
+            right++;
         }
 
-        return count;
+        return result;
     }
 };
-// @lc code=end
 
+// @lc code=end
 
 
 
@@ -108,42 +135,85 @@ protected:
 TEST_F(CLASS_NAME, __LINE__)
 {
     // Example 1:
-    // Input: colors = [0,1,0,1,0], k = 3
-    // Output: 3
+    // Input: word = "aeioqq", k = 1
+    // Output: 0
+    // Explanation:
+    // There is no substring with every vowel.
 
-    vector<int> colors = {0, 1, 0, 1, 0};
-    int k = 3;
-	EXPECT_EQ(this->s.numberOfAlternatingGroups(colors, k), 3);
+    string word = "aeioqq";
+    int k = 1;
+    EXPECT_EQ(this->s.countOfSubstrings(word, k), 0);
 }
 
 TEST_F(CLASS_NAME, __LINE__)
 {
     // Example 2:
-    // Input: colors = [0,1,0,0,1,0,1], k = 6
-    // Output: 2
+    // Input: word = "aeiou", k = 0
+    // Output: 1
+    // Explanation:
+    // The only substring with every vowel and zero consonants is word[0..4], which is "aeiou".
 
-    vector<int> colors = {0, 1, 0, 0, 1, 0, 1};
-    int k = 6;
-	EXPECT_EQ(this->s.numberOfAlternatingGroups(colors, k), 2);
+    string word = "aeiou";
+    int k = 0;
+    EXPECT_EQ(this->s.countOfSubstrings(word, k), 1);
 }
 
 TEST_F(CLASS_NAME, __LINE__)
 {
     // Example 3:
-    // Input: colors = [1,1,0,1], k = 4
-    // Output: 0
+    // Input: word = "ieaouqqieaouqq", k = 1
+    // Output: 3
+    // Explanation:
+    // The substrings with every vowel and one consonant are:
+    // word[0..5], which is "ieaouq".
+    // word[6..11], which is "qieaou".
+    // word[7..12], which is "ieaouq".
 
-    vector<int> colors = {1, 1, 0, 1};
-    int k = 4;
-	EXPECT_EQ(this->s.numberOfAlternatingGroups(colors, k), 0);
+    string word = "ieaouqqieaouqq";
+    int k = 1;
+    EXPECT_EQ(this->s.countOfSubstrings(word, k), 3);
 }
 
 TEST_F(CLASS_NAME, __LINE__)
 {
-    vector<int> colors = {0, 0, 1};
-    int k = 3;
-	EXPECT_EQ(this->s.numberOfAlternatingGroups(colors, k), 1);
+    string word = "kkaeiouk";
+    int k = 1;
+    EXPECT_EQ(this->s.countOfSubstrings(word, k), 2);
 }
+
+TEST_F(CLASS_NAME, __LINE__)
+{
+    string word = "kaeibouk";
+    int k = 1;
+    EXPECT_EQ(this->s.countOfSubstrings(word, k), 1);
+}
+
+TEST_F(CLASS_NAME, __LINE__)
+{
+    string word = "kaeiboauk";
+    int k = 1;
+    EXPECT_EQ(this->s.countOfSubstrings(word, k), 2);
+}
+
+TEST_F(CLASS_NAME, __LINE__)
+{
+    string word = "kaeiouuk";
+    int k = 1;
+    EXPECT_EQ(this->s.countOfSubstrings(word, k), 3);
+}
+
+TEST_F(CLASS_NAME, __LINE__)
+{
+    string word = "aeiokua";
+    int k = 1;
+    EXPECT_EQ(this->s.countOfSubstrings(word, k), 3);
+}
+
+
+
+
+
+
 
 
 int main(int argc, char **argv)
