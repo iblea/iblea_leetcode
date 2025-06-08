ae14a2151031761f64d5c79cf275e495a8719292
iblea <iblea@iasdf.com>
2025-06-08 12:40:53 +0900

    cpp leetcode template tab to space

--- template/leetcode_template.cpp
+++ template/leetcode_template.cpp
@@ -30,38 +30,37 @@ string Solution::solution() { return "template_function"; };
 
 class CLASS_NAME: public ::testing::Test {
 private:
-	clock_t t0;
-	clock_t t1;
+    clock_t t0;
+    clock_t t1;
 public:
-	Solution s;
+    Solution s;
 protected:
-	CLASS_NAME() { this->s = Solution(); }
-	~CLASS_NAME() override { }
+    CLASS_NAME() { this->s = Solution(); }
+    ~CLASS_NAME() override { }
 
-	void SetUp() override { set_start_time(); }
-	void TearDown() override { set_end_time(); get_duration_time(); }
+    void SetUp() override { set_start_time(); }
+    void TearDown() override { set_end_time(); get_duration_time(); }
 
-	void set_start_time() {
-		this->t0 = clock(); // or gettimeofday or whatever
-	}
-	void set_end_time() {
-		this->t1 = clock(); // or gettimeofday or whatever
-	}
+    void set_start_time() {
+        this->t0 = clock(); // or gettimeofday or whatever
+    }
+    void set_end_time() {
+        this->t1 = clock(); // or gettimeofday or whatever
+    }
 
-	// 시간 관련 체크
-	void get_duration_time() {
-		double elapsedSec = (double)(this->t1 - this->t0) / (double)1000000;
-		double elapsedMS = (double)(this->t1 - this->t0) / (double)1000;
+    // 시간 관련 체크
+    void get_duration_time() {
+        double elapsedSec = (double)(this->t1 - this->t0) / (double)1000000;
+        double elapsedMS = (double)(this->t1 - this->t0) / (double)1000;
 
-		cout << setprecision(3) << fixed << endl << "Duration of Times : ";
-		cout << elapsedMS << "ms, " << elapsedSec << "sec" << endl << endl;
-	}
-
+        cout << setprecision(3) << fixed << endl << "Duration of Times : ";
+        cout << elapsedMS << "ms, " << elapsedSec << "sec" << endl << endl;
+    }
 };
 
 TEST_F(CLASS_NAME, __LINE__)
 {
-	EXPECT_EQ(this->s.solution(), "template_function");
+    EXPECT_EQ(this->s.solution(), "template_function");
 }
 
 int main(int argc, char **argv)
