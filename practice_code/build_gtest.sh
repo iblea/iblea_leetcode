9131b2570b75f9963925f90c7414d58ed083115f
iblea <iblea@iasdf.com>
2026-03-16 18:17:23 +0900

    build_gtest 수정

--- practice_code/build_gtest.sh
+++ practice_code/build_gtest.sh
@@ -26,18 +26,32 @@ fi
 # g++
 # /opt/homebrew/bin/g++-13
 
-clang++ -Wall -g -O0 \
-	-I $gtest_path/include \
-	-L $gtest_path/lib \
-	-std=c++17 \
-	$fileName \
-	-o $binName \
-	-lgtest -lgtest_main -lpthread \
-	-fno-omit-frame-pointer \
-	-gdwarf-4
+
+COMPILER=""
+
+if [ -n "$(which clang++)" ]; then
+    COMPILER="clang++"
+elif [ -n "$(which g++)" ]; then
+    COMPILER="g++"
+else
+    echo "clang++ / g++ not found."
+    exit 1
+fi
+
+g++ -Wall -g -O0 \
+    -I $gtest_path/include \
+    -L $gtest_path/lib \
+    -std=c++17 \
+    $fileName \
+    -o $binName \
+    -lgtest -lgtest_main -lpthread \
+    -fno-omit-frame-pointer \
+    -gdwarf-4
+
+
 
 
 # 이 디렉토리 없애면 중단점 등 안됨.
 # if [ -d "$binName.dSYM/" ]; then
-# 	rm -rf ./$binName.dSYM/
+#     rm -rf ./$binName.dSYM/
 # fi
