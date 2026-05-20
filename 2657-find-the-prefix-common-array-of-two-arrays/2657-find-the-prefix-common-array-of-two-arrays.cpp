254fdbf02c106da55eabe74889e92002380ef912
Iblea <iblea@iasdf.com>
2026-05-20 14:00:41 +0900

    Time: 0 ms (100%), Space: 85.8 MB (76.5%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,30 +1,49 @@
 class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-        size_t n = A.size();
-        set<int> seta;
-        vector<int> result(n, 0);
+        int n = A.size();
+        vector<int> count(n+1,-1);
+        vector<int> C(n);
 
-        seta.insert(A[0]);
-        if (seta.find(B[0]) != seta.end()) {
-            result[0]++;
-        } else {
-            seta.insert(B[0]);
+        int target;
+        for (int i = 0 ; i < n; i++) {
+            target = A[i];
+            count[target] = i;
         }
-
-        for (int i = 1; i < n; i++) {
-            result[i] = result[i-1];
-            if (seta.find(B[i]) != seta.end()) {
-                result[i]++;
-            } else {
-                seta.insert(B[i]);
-            }
-            if (seta.find(A[i]) != seta.end()) {
-                result[i]++;
-            } else {
-                seta.insert(A[i]);
+        for (int i = 0; i < n; i++) {
+            target = B[i];
+            if (count[target] < i) {
+                count[target] = i;
             }
         }
-        return result;
+
+        int common = 0;
+        for (int i = 0; i < n; i++) {
+            if (count[A[i]] <= i) {
+                common++;
+            }
+            if (A[i] != B[i] && count[B[i]] <= i) {
+                common++;
+            }
+            C[i] = common;
+        }
+
+        return C;
+        
     }
+
+    vector<int> findThePrefixCommonArray2(vector<int>& A, vector<int>& B) {
+        int n = A.size();
+        vector<int> count(n + 1, 0);
+        vector<int> C(n);
+        int common = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (++count[A[i]] == 2) common++;  // A[i] 등장 → 2면 공통
+            if (++count[B[i]] == 2) common++;  // B[i] 등장 → 2면 공통
+            C[i] = common;
+        }
+        return C;
+    }
+
 };
